8dfbeb1a95129d1cb20778e59a1dae59e0da27c4
David Luz <davidmelo8@hotmail.com>
2015-04-28 17:53:14 -0300

    v.1.0.3

--- js/main.js
+++ js/main.js
@@ -1,31 +1,61 @@
+var riQuestions = [];
+var questionAtual = 1;
+
+
+
+var Question = function(){
+$("#question-container").append('<h3>Questão 1</h3><p>Este é um texto<input id="ri-resposta" class="textbox" type="text" value="valor">.</input></p>');
+$("#question-container").append('<button id="ri-send-button">Enviar</button>');
+}
+
+
+
+
+
+// Main Function
 $(document).ready(function() {
+var questao1 = new Question();	
+console.log("Running!");
+riProgresseBar('3');
 
-// Container View
-var ContainerView = Backbone.View.extend({
-el: $('#ri-container'),
-initialize: function() {
-this.render();
-},
-render: function() {
-$(this.el).append("<h1>Hello Moodle!</h1>");
+$("#ri-send-button").click(function() {
+var value = $( "#ri-resposta" ).val();
+if(value=="teste"){
+alert('você acertou');
 }
-});
 
-//Dependencies
-var Dependencies = Backbone.View.extend({
-el: $('#ri-dependencies'),
-initialize: function() {
-this.render();
-},
-render: function() {
-$(this.el).append("<script>js/arquivo.js</script>");
+else{
+alert("você errou");
 }
+ 
+ 
 });
 
 
-var helloView = new ContainerView();
-var Dependencia1 = new Dependencies();
+function riProgresseBar(next){
+$('#ri-progress-bar').jQMeter({
+  goal:'5',
+  raised: next,
+  width:'300px',
+  barColor: '#00BD9B',
+  bgColor: "#E8EDF2",
+  height: '10px'
+});
+}
+
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
