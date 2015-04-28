3ec81ca35fdd61bff5a4382db24195a8602081ff
David Luz <davidmelo8@hotmail.com>
2015-04-28 13:45:09 -0300

    V.1.0.2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
+
+// Container View
 var ContainerView = Backbone.View.extend({
 el: $('#ri-container'),
-
 initialize: function() {
 this.render();
 },
-
 render: function() {
 $(this.el).append("<h1>Hello Moodle!</h1>");
 }
 });
 
-var helloView = new ContainerView();
+//Dependencies
+var Dependencies = Backbone.View.extend({
+el: $('#ri-dependencies'),
+initialize: function() {
+this.render();
+},
+render: function() {
+$(this.el).append("<script>js/arquivo.js</script>");
+}
+});
 
+
+var helloView = new ContainerView();
+var Dependencia1 = new Dependencies();
 
 });
+
+
