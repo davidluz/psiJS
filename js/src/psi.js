c2d52b01fdee82ad4b84fe3e8b75967845adf3bf
davidluz <davidmelo8@hotmail.com>
2015-09-18 15:23:08 -0300

    v 0.0.2
    v 0.0.2

--- js/src/psi.js
+++ js/src/psi.js
@@ -1,9 +1,17 @@
 //Statics functions
 
+
+
+
 // Receive objects with number of responses 
 // Return
 // Type - Render
 function GraphDefault(Responses,divName){
 	$(divName).append
 	return 
+}
+
+function  GraphCustom (Response, divName){
+	$(divName).append
+	return
 }
